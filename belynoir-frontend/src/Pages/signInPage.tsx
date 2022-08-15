ca840805e1b045d46bad03feeb812f48c24a0607
Diego Bascans <diegobascans@hotmail.com>
2022-08-15 20:33:38 -0300

    Adding missing React import

--- belynoir-frontend/src/Pages/signInPage.tsx
+++ belynoir-frontend/src/Pages/signInPage.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import SignIn from "../Components/SignIn/signIn";
 
 
 const SignInPage = () => {
     return (
-        <SignIn></SignIn>
+        <SignIn />
     )
 }
 
